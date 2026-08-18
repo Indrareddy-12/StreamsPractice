45be231c41b0cd07f0ce96b3c5abcf2ac0f2df7b
Indrareddy-12 <147275226+Indrareddy-12@users.noreply.github.com>
2026-08-18 18:33:29 +0530

    Add debugging comment in Main.java
    added some comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        //hellohi
 
 //        Map<Thread, StackTraceElement[]> threads = Thread.getAllStackTraces();
 //
